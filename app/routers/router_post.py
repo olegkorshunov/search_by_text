6dba9c8281b45e24e0b173c4aef030b52d55a04f
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2022-05-31 03:24:53 +0300

    'naming'

--- app/routers/router_post.py
+++ app/routers/router_post.py
@@ -8,7 +8,7 @@ router_post = APIRouter(prefix="/post")
 @router_post.get(
     "/{post_id}", response_model=schemas.Post, status_code=status.HTTP_200_OK
 )
-async def read_user(post_id: int):
+async def read_post(post_id: int):
     db_post = await crud_post.get_post(post_id=post_id)
     if db_post is None:
         raise HTTPException(status_code=404, detail="Post not found")
